a57bc9327ef4676b2ab3362e7bd3ec7a34aa32e6
RaisulislamRimon <rimonppip0@gmail.com>
2022-10-26 23:21:59 +0600

    project initialized

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 5000;
+
+const courses = require("./allCourses.json");
+
+app.get("/", (req, res) => res.send("Hello World!"));
+
+app.get("/all-courses", (req, res) => {
+  res.send(courses);
+});
+
+app.get("/course/:id", (req, res) => {
+  // res.send("Course ID: " + req.params.id);
+  const id = req.params.id;
+  const course = courses.find((course) => course.id === id);
+  res.send(course);
+});
+
+app.use((req, res, next) => {
+  res.status(404).send("Sorry can't find that!");
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
